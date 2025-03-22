3397fd34fe535ed75874eaf50a025c79a069d8f8
Kojirou <54878605+KameiKojirou@users.noreply.github.com>
2025-03-22 14:14:29 -0600

    very simple multiplayer gdscript

---
+++ basic_player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@onready var camera: Camera2D = $Camera2D
+
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+
+func _ready():
+	if is_multiplayer_authority():
+		camera.make_current()
+
+func _physics_process(delta: float) -> void:
+	if is_multiplayer_authority():
+		velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 400
+	move_and_slide()
